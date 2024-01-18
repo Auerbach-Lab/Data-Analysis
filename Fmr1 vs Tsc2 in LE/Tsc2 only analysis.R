f000f1116a48050faee00001e8ed2cdef6010303
Noelle James <github@nohelix.com>
2024-01-18 16:35:04 -0600

    Reaction graph improvements
    mean now calculated with na.rm, group as well as overall lines drawn.

--- Fmr1 vs Tsc2 in LE/Tsc2 only analysis.R
+++ Fmr1 vs Tsc2 in LE/Tsc2 only analysis.R
@@ -172,7 +172,7 @@ single_Frequency = "BBN"
 Rxn_table %>%
   {if (drop_TP3) filter(., rat_name != "TP3")} %>%
   filter(line == "Tsc2-LE") %>%
-  filter(Duration %in% c(300)) %>%
+  filter(Duration %in% c(300, 100)) %>%
   # rename(Intensity = `Inten (dB)`) %>%
   filter(detail == "Alone") %>%
   mutate(group = if_else(rat_ID < 300, "Group 1", "Group 2")) %>%
@@ -183,28 +183,35 @@ Rxn_table %>%
   filter(Frequency == single_Frequency) %>%
   ggplot(aes(x = Intensity, y = Rxn, linetype = as.factor(group),
              color = genotype, group = interaction(Duration, group, genotype))) +
-  stat_summary(fun = mean,
-               fun.min = function(x) mean(x) - se(x),
-               fun.max = function(x) mean(x) + se(x),
+  stat_summary(aes(x = Intensity, y = Rxn,color = genotype,group = genotype),
+               fun = function(x) mean(x, na.rm = TRUE),
+               fun.min = function(x) mean(x, na.rm = TRUE) - se(x),
+               fun.max = function(x) mean(x, na.rm = TRUE) + se(x),
+               geom = "errorbar", width = 1.5, linetype = "solid", linewidth = 1.5, alpha = 0.5) +
+  stat_summary(aes(x = Intensity, y = Rxn,color = genotype,group = genotype),
+               fun = function(x) mean(x, na.rm = TRUE),
+               geom = "line", linetype = "solid", linewidth = 1.5, alpha = 0.5) +
+  stat_summary(fun = function(x) mean(x, na.rm = TRUE),
+               fun.min = function(x) mean(x, na.rm = TRUE) - se(x),
+               fun.max = function(x) mean(x, na.rm = TRUE) + se(x),
                geom = "errorbar", width = 1.5, position = position_dodge(1)) +
-  stat_summary(fun = mean,
+  stat_summary(fun = function(x) mean(x, na.rm = TRUE),
                geom = "point", position = position_dodge(1), size = 3) +
-  stat_summary(fun = mean, geom = "line", position = position_dodge(1)) +
+  stat_summary(fun = function(x) mean(x, na.rm = TRUE), 
+               geom = "line", position = position_dodge(1)) +
   labs(x = "Intensity (dB)",
        y = "Reaction time (ms, mean +/- SE)",
        color = "Genotype", linetype = "",
        caption = if_else(drop_TP3, "Without Het F TP3", "With TP3")) +
-  # scale_linetype_manual(values = c("Tsc2-LE" = "solid", "Tsc2-LE" = "longdash")) +
+  scale_linetype_manual(values = c("Group 1" = "solid", "Group 2" = "longdash")) +
   scale_color_manual(values = c("WT" = "black", "Het" = "deepskyblue", "KO" = "red")) +
   scale_x_continuous(breaks = seq(0, 90, by = 10)) +
-  facet_wrap(~ Duration) +
+  facet_wrap(~ factor(Duration, levels = c(300, 100))) +
   theme_classic() +
   theme(
     plot.title = element_text(hjust = 0.5),
     panel.grid.major.x = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
   ) 
-  # theme(legend.position = c(0.88, 0.8),
-  #       legend.background=element_blank())
 
 # ggsave(filename = paste0("Tsc2_Rxn_all_freq.jpg"),
 # plot = last_plot(),
