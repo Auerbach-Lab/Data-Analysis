20ddb7fe439f3b123900c758fd94c0ff11a7dabf
Noelle James <github@nohelix.com>
2024-01-24 16:56:11 -0600

    Moved groups to main
    Now used in more than one script.

--- TTS/Temporal Integration.R
+++ TTS/Temporal Integration.R
@@ -1,23 +1,4 @@
 
-# Groupings ---------------------------------------------------------------
-
-# TTS Pilot Group - 50ms data only b/c mostly mixed
-# 2 rats "Green2" (16) & "Orange1" (21) have incomplete data set
-Group_TTS_pilot = c(15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
-
-# Temporal Integration Pilot Group - all 3 time points and ABRs pre & post HL
-# 1 rat "Green12" (191) had threshold shift
-Group_1 = c(190, 191, 192, 193)
-
-# Temporal Integration Pilot Group - all 3 time points and ABRs pre & post HL
-# 1 rat "Blue2" (266) died during baseline
-# 1 rat "Blue3" (267) never got to FA criterion and was dropped
-Group_2 = c(265, 266, 267, 268)
-
-# Temporal Integration Pilot Group - all 3 time points and ABRs pre & post HL
-# in progress
-Group_3 = c(338, 339, 340, 341, 342, 343)
-
 # Get Data ----------------------------------------------------------------
 
 # Get pilot BBN threshold data
