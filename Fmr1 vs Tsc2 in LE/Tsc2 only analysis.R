199727f9939a1800d544e5aee52d920528a19d18
Noelle James <github@nohelix.com>
2024-05-30 10:56:56 -0500

    Many changes that should have been multiple commits
    added multiple seizure rat handling
    Fixed TH stats testing
    working on Rxn stats

--- Fmr1 vs Tsc2 in LE/Tsc2 only analysis.R
+++ Fmr1 vs Tsc2 in LE/Tsc2 only analysis.R
@@ -5,7 +5,8 @@
 
 # Variables ---------------------------------------------------------------
 
-drop_TP3 = TRUE
+drop_seizure_rats = FALSE
+rats_with_spontanious_seizures = c("TP3", "Blue3")
 
 # Graphing ----------------------------------------------------------------
 # Calculate standard error (SE) like standard deviation (SD)
@@ -26,99 +27,102 @@ Parametric_Check <- function(AOV.data) {
 }
 
 
-# TH Averages -------------------------------------------------------------
-Tsc2_TH_averages = TH_table %>%
+# TH analysis -------------------------------------------------------------
+# note that for Tukey everything needs to be a factor and Duration (a number
+# value) tries to make itself continuous
+
+#BBN only with duration & detail
+Tsc2.TH.aov.data = TH_table %>%
   filter(line == "Tsc2-LE") %>%
-  group_by(genotype, detail, Frequency , Duration) %>%
-  summarise(TH = mean(TH, na.rm = TRUE), .groups = "drop")
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else (.)} %>%
+  filter(Frequency == 0) %>%
+  filter(detail %in% c("Alone", "Recheck")) %>%
+  mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
+                           rat_ID < 300 ~ "Group 1",
+                           rat_ID >= 300 ~ "Group 2",
+                           .default = "Unknown")) %>%
+  filter(group %in% c("Group 1", "Group 2 Recheck")) %>%
+  filter(Duration %in% c(300, 50)) %>% # Recheck was only on 50 & 300
+  mutate(genotype = as.factor(genotype),
+         Duration = as.factor(Duration),
+         sex = as.factor(sex))
 
+Tsc2.TH.aov.data$Gaus = LambertW::Gaussianize(Tsc2.TH.aov.data$TH)[, 1]
 
-# # TH analysis -------------------------------------------------------------
-# # note that for Tukey everything needs to be a factor and Duration (a number
-# # value) tries to make itself continuous
-# 
-# Tsc2.TH.aov.data = TH_table %>%
-#   filter(line == "Tsc2-LE") %>%
-#   filter(Duration == "50" & detail == "Alone") %>%
-#   # Getting an NA from TP5 so drop
-#   filter(! (rat_name == "TP5" & Frequency == "4"))
-# 
-# Tsc2.TH.aov.data$Gaus = LambertW::Gaussianize(Tsc2.TH.aov.data$TH)[, 1]
-# 
-# Tsc2.TH.aov = aov(Gaus ~ genotype * as.factor(Frequency), 
-#                   data = Tsc2.TH.aov.data)
-# 
-# Parametric_Check(Tsc2.TH.aov)
-# 
-# # Non-Normal
-# # summary(Tsc2.TH.aov)
-# 
-# # Kruskal Testing - only 
-# lapply(c("Frequency", "genotype"), 
-#        function(x) kruskal.test(reformulate(x, "TH"), data = Tsc2.TH.aov.data)) %>% 
+Tsc2.TH.aov = aov(Gaus ~ genotype * Duration * sex,
+                  data = Tsc2.TH.aov.data)
+
+Parametric_Check(Tsc2.TH.aov)
+
+# Non-Normal
+summary(Tsc2.TH.aov)
+
+# # Kruskal Testing - only
+# lapply(c("Frequency", "genotype", "Duration"),
+#        function(x) kruskal.test(reformulate(x, "TH"), data = Tsc2.TH.aov.data)) %>%
 #   # Convert to table
 #   do.call(rbind, .) %>% as_tibble() %>% mutate_all(unlist) %>%
 #   # do a p adjustment and then sig label
 #   mutate(adj.p.value = p.adjust(p.value, "BH"),
 #          sig = gtools::stars.pval(adj.p.value))
-# 
-# # No effect on thresholds
-# 
-# # TODO: BBN only with duration & detail
-# 
-# Tsc2_TH_averages %>%
-#   filter(Frequency == 0) %>%
-#   # Combine Frequency and Duration to create a single key column
-#   mutate(key = paste0(Frequency, "kHz", "_", Duration, "ms")) %>%
-#   # Remove redundant columns
-#   select(-all_of(c("Frequency", "Duration"))) %>%
-#   spread(key, TH)
-# 
-# # TH Duration --------------------------------------------------------------
-# 
-# Tsc2.TH.BBN.aov.data = TH_table %>%
-#   filter(line == "Tsc2-LE") %>%
-#   filter(detail != "Rotating" & Frequency == 0) 
-# 
-# Tsc2.TH.BBN.aov.data$Gaus = LambertW::Gaussianize(Tsc2.TH.BBN.aov.data$TH)[, 1]
-# 
-# Tsc2.TH.BBN.aov = aov(TH ~ detail * Duration * genotype,
-#                       data = Tsc2.TH.BBN.aov.data)
-# 
-# Parametric_Check(Tsc2.TH.BBN.aov)
-# 
-# # Normal
-# summary(Tsc2.TH.BBN.aov)
-# 
-# # Only primary effects so no post-Hoc needed
+
+broom::tidy(TukeyHSD(Tsc2.TH.aov)) %>% mutate(sig = gtools::stars.pval(adj.p.value)) %>%
+  filter(sig != " " & str_detect(term, pattern = ":", negate = TRUE)) 
+
+
+# No sex or genotyp effect on thresholds
+
+# TH Averages -------------------------------------------------------------
+
+Tsc2_TH_averages = 
+  TH_table %>%
+  filter(line == "Tsc2-LE") %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else (.)} %>%
+  filter(Frequency == 0) %>%
+  filter(detail %in% c("Alone", "Recheck")) %>%
+  mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
+                           rat_ID < 300 ~ "Group 1",
+                           rat_ID >= 300 ~ "Group 2",
+                           .default = "Unknown")) 
+
+# View
+Tsc2_TH_averages %>%
+  filter(group %in% c("Group 1", "Group 2 Recheck")) %>%
+  filter(Duration %in% c(300, 50)) %>%
+  group_by(genotype, Frequency, group, Duration) %>%
+  summarise(TH = mean(TH, na.rm = TRUE), .groups = "drop") %>%
+  # Combine Frequency and Duration to create a single key column
+  mutate(key = paste0(Frequency, "kHz", "_", Duration, "ms")) %>%
+  # Remove redundant columns
+  select(-all_of(c("Frequency", "Duration"))) %>%
+  spread(key, TH)
 
 
 # TH Graph ----------------------------------------------------------------
 
 Tsc2_TH_gaph = 
   TH_table %>%
-    filter(line == "Tsc2-LE") %>%
-    filter(Frequency == 0) %>%
-    filter(detail %in% c("Alone", "Recheck")) %>%
-    {if (drop_TP3) filter(., rat_name != "TP3")} %>%
-    mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
-                             rat_ID < 300 ~ "Group 1",
-                             rat_ID >= 300 ~ "Group 2",
-                             .default = "Unknown")) %>%
-    mutate(Frequency = str_replace_all(Frequency, pattern = "0", replacement = "BBN") %>% 
-             factor(levels = c("BBN", "4", "8", "16", "32"))) %>%
-    ggplot(aes(x = genotype, y = TH, shape = line,
-               fill = genotype, color = group, group = interaction(group, line, genotype))) +
+  filter(line == "Tsc2-LE") %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else (.)} %>%
+  filter(Frequency == 0) %>%
+  filter(detail %in% c("Alone", "Recheck")) %>%
+  mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
+                           rat_ID < 300 ~ "Group 1",
+                           rat_ID >= 300 ~ "Group 2",
+                           .default = "Unknown")) %>%
+  mutate(Frequency = str_replace_all(Frequency, pattern = "0", replacement = "BBN") %>% 
+           factor(levels = c("BBN", "4", "8", "16", "32"))) %>%
+  ggplot(aes(x = genotype, y = TH, shape = line,
+             fill = genotype, color = group, group = interaction(group, line, genotype))) +
     geom_boxplot(position = position_dodge(1), linewidth = 1, width = 0.8) +
     # geom_point(aes(color = genotype), alpha = 0.3, position = position_dodge(1)) +
     stat_summary(fun.data = n_fun, geom = "text", show.legend = FALSE, 
                  position = position_dodge(1), vjust = 2, size = 3) +
-    # scale_color_manual(values = c("Tsc2-LE" = "darkblue", "Fmr1-LE" = "red")) +
     scale_color_manual(values = c("Group 1" = "darkblue", "Group 2" = "goldenrod", "Group 2 Recheck" = "green")) +
-    scale_fill_manual(values = c("WT" = "black", "Het" = "deepskyblue", "KO" = "lightcoral")) +
+    scale_fill_manual(values = c("WT" = "grey40", "Het" = "deepskyblue", "KO" = "lightcoral")) +
     labs(x = "",
          y = "Threshold (dB, mean +/- SE)",
-         caption = if_else(drop_TP3, "Without Het F TP3", "With TP3"),
+         caption = if_else(drop_seizure_rats, "Without Female rats with spontanious seizures", "All rats"),
          fill = "Genotype") +
     facet_wrap( ~ Duration, ncol = 5, scales = "free_x") +
     theme_classic() +
@@ -129,28 +133,72 @@ Tsc2_TH_gaph =
 
 print(Tsc2_TH_gaph)
 
+Tsc2_TH_overview_gaph = 
+  TH_table %>%
+  filter(line == "Tsc2-LE") %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else (.)} %>%
+  filter(Frequency == 0) %>%
+  filter(detail %in% c("Alone", "Recheck")) %>%
+  mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
+                           rat_ID < 300 ~ "Group 1",
+                           rat_ID >= 300 ~ "Group 2",
+                           .default = "Unknown")) %>%
+  filter(group %in% c("Group 1", "Group 2 Recheck")) %>%
+  mutate(Frequency = str_replace_all(Frequency, pattern = "0", replacement = "BBN") %>% 
+           factor(levels = c("BBN", "4", "8", "16", "32"))) %>%
+  ggplot(aes(x = genotype, y = TH, fill = genotype, group = interaction(genotype))) +
+    geom_boxplot(position = position_dodge(1), linewidth = 1, width = 0.8) +
+    stat_summary(aes(color = sex, group = interaction(sex, genotype)),
+                 fun = function(x) mean(x, na.rm = TRUE),
+                 geom = "point", show.legend = FALSE, size = 3) +
+    stat_summary(fun.data = n_fun, geom = "text", show.legend = FALSE, 
+                 position = position_dodge(1), vjust = 2, size = 3) +
+    # scale_color_manual(values = c("Tsc2-LE" = "darkblue", "Fmr1-LE" = "red")) +
+    scale_color_manual(values = c("Male" = "blue", "Female" = "red")) +
+    scale_fill_manual(values = c("WT" = "grey40", "Het" = "deepskyblue", "KO" = "lightcoral")) +
+    labs(x = "",
+         y = "Threshold (dB, mean +/- SE)",
+         caption = if_else(drop_seizure_rats, "Without Female rats with spontanious seizures", "All rats"),
+         fill = "Tsc2\nGenotype") +
+    facet_wrap( ~ Duration, ncol = 5, scales = "free_x") +
+    theme_classic() +
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      panel.grid.major.y = element_line(color = rgb(235, 235, 235, 255, maxColorValue = 255))
+    )
+
+print(Tsc2_TH_overview_gaph)
+
 # Rxn analysis ------------------------------------------------------------
 # Get data
-Tsc2_Rxn_over_TH = filter(Rxn_table_over_TH, line == "Tsc2-LE")
+Tsc2_Rxn_over_TH = Rxn_table_over_TH %>%
+  filter(line == "Tsc2-LE") %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else (.)} %>%
+  filter(detail %in% c("Alone", "Recheck")) %>%
+  mutate(group = case_when(detail == "Recheck" ~ "Group 2 Recheck",
+                           rat_ID < 300 ~ "Group 1",
+                           rat_ID >= 300 ~ "Group 2",
+                           .default = "Unknown")) %>%
+  filter(group %in% c("Group 1", "Group 2 Recheck"))
 
 Tsc2_Rxn_over_TH$Gaus = LambertW::Gaussianize(Tsc2_Rxn_over_TH$Rxn)[, 1]
 
 
-## Overall Model
-  Tsc2_Rxn_overall_model = aov(Gaus ~ Frequency * Intensity * genotype * sex,
-                               data = filter(Tsc2_Rxn_over_TH, detail == "Alone" & Duration == "50"))
-  # Normality testing
-  Parametric_Check(Tsc2_Rxn_overall_model)
-  
-  # Non-normal even with transformation
-  kruskal.test(Rxn ~ genotype,
-               data = filter(Tsc2_Rxn_over_TH, detail == "Alone" & Duration == "50"))
-  
-  kruskal.test(Rxn ~ Frequency,
-               data = filter(Tsc2_Rxn_over_TH, detail == "Alone" & Duration == "50"))
+## Overall Model ----
+  # Tsc2_Rxn_overall_model = aov(Gaus ~ Intensity * genotype * sex * Duration,
+  #                              data = Tsc2_Rxn_over_TH)
+  # # Normality testing
+  # Parametric_Check(Tsc2_Rxn_overall_model)
+  # 
+  # # Non-normal even with transformation
+  # kruskal.test(Rxn ~ genotype,
+  #              data = filter(Tsc2_Rxn_over_TH, detail == "Alone" & Duration == "50"))
+  # 
+  # kruskal.test(Rxn ~ Frequency,
+  #              data = filter(Tsc2_Rxn_over_TH, detail == "Alone" & Duration == "50"))
 
 
-## BBN Model
+## BBN Model ----
   Tsc2.Rxn.BBN.aov.data = Tsc2_Rxn_over_TH %>%
     filter(Frequency == 0)
   
@@ -163,36 +211,37 @@ Tsc2_Rxn_over_TH$Gaus = LambertW::Gaussianize(Tsc2_Rxn_over_TH$Rxn)[, 1]
   # Not even close to normal
   # summary(Tsc2.Rxn.BBN.aov)
   
-  # Kruskal Testing - only 
-  lapply(c("genotype", "sex", "interaction(genotype, sex)"), 
+  # Kruskal Testing - Main effects only 
+  lapply(c("genotype", "sex", "Duration"), 
          function(x) kruskal.test(reformulate(x, "Rxn"),
-                                  data = filter(Tsc2.Rxn.BBN.aov.data, detail == "Alone"))) %>% 
+                                  data = filter(Tsc2.Rxn.BBN.aov.data))) %>% 
     # Convert to table
     do.call(rbind, .) %>% as_tibble() %>% mutate_all(unlist) %>%
     # do a p adjustment and then sig label
     mutate(adj.p.value = p.adjust(p.value, "bonf"),
-           sig = gtools::stars.pval(adj.p.value))
+           sig = gtools::stars.pval(adj.p.value)) %>%
+    select(method, parameter, statistic, data.name, adj.p.value, sig)
 
-  # Post-Hoc Dunn's Test
-  FSA::dunnTest(Rxn ~ interaction(Duration, genotype, sex), 
-                  data = filter(Tsc2.Rxn.BBN.aov.data, detail == "Alone"),
-                  method = "bonf") %>%
-    .$res %>%
-    as_tibble() %>%
-    select(-P.unadj) %>%
-    mutate(Sig = gtools::stars.pval(P.adj),
-           Comp1 = str_split_fixed(.$Comparison, ' - ', 2)[,1],
-           Comp2 = str_split_fixed(.$Comparison, ' - ', 2)[,2],
-           dur1 = str_split_fixed(Comp1, '\\.', 3)[,1] %>% as.numeric(),
-           geno1 = str_split_fixed(Comp1, '\\.', 3)[,2],
-           sex1 = str_split_fixed(Comp1, '\\.', 3)[,3],
-           dur2 = str_split_fixed(Comp2, '\\.', 3)[,1] %>% as.numeric(),
-           geno2 = str_split_fixed(Comp2, '\\.', 3)[,2],
-           sex2 = str_split_fixed(Comp2, '\\.', 3)[,3]) %>%
-    filter(dur1 == dur2) %>%
-    filter(! Sig %in% c(" ", ".")) %>%
-    arrange(dur1) %>%
-    select(-Comparison, -Comp1, -Comp2, -dur2) 
+  # # Post-Hoc Dunn's Test
+  # FSA::dunnTest(Rxn ~ interaction(Duration, genotype, sex), 
+  #                 data = filter(Tsc2.Rxn.BBN.aov.data, detail == "Alone"),
+  #                 method = "bonf") %>%
+  #   .$res %>%
+  #   as_tibble() %>%
+  #   select(-P.unadj) %>%
+  #   mutate(Sig = gtools::stars.pval(P.adj),
+  #          Comp1 = str_split_fixed(.$Comparison, ' - ', 2)[,1],
+  #          Comp2 = str_split_fixed(.$Comparison, ' - ', 2)[,2],
+  #          dur1 = str_split_fixed(Comp1, '\\.', 3)[,1] %>% as.numeric(),
+  #          geno1 = str_split_fixed(Comp1, '\\.', 3)[,2],
+  #          sex1 = str_split_fixed(Comp1, '\\.', 3)[,3],
+  #          dur2 = str_split_fixed(Comp2, '\\.', 3)[,1] %>% as.numeric(),
+  #          geno2 = str_split_fixed(Comp2, '\\.', 3)[,2],
+  #          sex2 = str_split_fixed(Comp2, '\\.', 3)[,3]) %>%
+  #   filter(dur1 == dur2) %>%
+  #   filter(! Sig %in% c(" ", ".")) %>%
+  #   arrange(dur1) %>%
+  #   select(-Comparison, -Comp1, -Comp2, -dur2) 
     
   
 
@@ -204,7 +253,7 @@ single_Frequency = "BBN"
 
 Tsc_single_frequency_rxn_graph =   
   Rxn_table %>%
-    {if (drop_TP3) filter(., rat_name != "TP3")} %>%
+    {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else .} %>%
     filter(line == "Tsc2-LE") %>%
     filter(Duration %in% c(50, 100, 300)) %>%
     # rename(Intensity = `Inten (dB)`) %>%
@@ -240,7 +289,7 @@ Tsc_single_frequency_rxn_graph =
     labs(x = "Intensity (dB)",
          y = "Reaction time (ms, mean +/- SE)",
          color = "Genotype", linetype = "",
-         caption = if_else(drop_TP3, "Without Het F TP3", "With TP3")) +
+         caption = if_else(drop_seizure_rats, "Without Female rats with spontanious seizures", "All rats")) +
     scale_linetype_manual(values = c("Group 1" = "solid", "Group 2" = "longdash")) +
     scale_color_manual(values = c("WT" = "black", "Het" = "deepskyblue", "KO" = "red")) +
     scale_x_continuous(breaks = seq(0, 90, by = 10)) +
@@ -262,7 +311,7 @@ print(Tsc_single_frequency_rxn_graph)
 
 Tsc_sex_differences_rxn_graph =   
   Rxn_table %>%
-  {if (drop_TP3) filter(., rat_name != "TP3")} %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else .} %>%
   filter(line == "Tsc2-LE") %>%
   filter(Duration %in% c(50, 100, 300)) %>%
   # rename(Intensity = `Inten (dB)`) %>%
@@ -298,7 +347,7 @@ Tsc_sex_differences_rxn_graph =
   labs(x = "Intensity (dB)",
        y = "Reaction time (ms, mean +/- SE)",
        color = "Genotype", linetype = "",
-       caption = if_else(drop_TP3, "Without Het F TP3", "With TP3")) +
+       caption = if_else(drop_seizure_rats, "Without Het F TP3", "With TP3")) +
   # scale_linetype_manual(values = c("Group 1" = "solid", "Group 2" = "longdash")) +
   scale_color_manual(values = c("WT" = "black", "Het" = "deepskyblue", "KO" = "red")) +
   scale_x_continuous(breaks = seq(0, 90, by = 10)) +
@@ -316,7 +365,7 @@ print(Tsc_sex_differences_rxn_graph)
 
 BBN_Individual_Graphs = 
 Rxn_table %>%
-  {if (drop_TP3) filter(., rat_name != "TP3")} %>%
+  {if (drop_seizure_rats) filter(., !(rat_name %in% rats_with_spontanious_seizures)) else .} %>%
   filter(line == "Tsc2-LE") %>%
   # filter(Duration %in% c(300, 100)) %>%
   filter(detail == "Alone") %>%
