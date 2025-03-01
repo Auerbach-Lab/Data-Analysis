a81fe86d1c7b71d602fdfdc4a4d020e7f1437f3a
Noelle James <github@nohelix.com>
2025-03-01 10:41:32 -0600

    Graphing of Background
    Considering redoing this in new group so wanted to see prior piloting results.

--- Fmr1 vs Tsc2 in LE/Fmr1 vs Tsc2 in LE Octave analysis.R
+++ Fmr1 vs Tsc2 in LE/Fmr1 vs Tsc2 in LE Octave analysis.R
@@ -374,6 +374,51 @@ Octave_graph_by_Range =
 
 # print(Octave_graph_by_Range)
 
+## Background ----
+BG_rats = filter(discrimination_data,
+                 str_detect(detail, pattern = "BG")) %>%
+  .$rat_ID %>% unique()
+
+ggplot(filter(discrimination_FA_table, rat_ID %in% BG_rats), 
+       aes(x = octave_steps, y = FA_percent_detailed * 100,
+           color = genotype, fill = line, linetype = detail,
+           group = interaction(detail, line, genotype))) +
+  geom_hline(yintercept = 50, color = "forestgreen", linewidth = 1.5) +
+  # mean for genotypes across all frequencies
+  stat_summary(fun = mean, geom = "line", linewidth = 1.5, position = position_dodge(.2)) +
+  # mean for each frequency by genotype
+  stat_summary(aes(shape = line), fun = mean, geom = "point",
+               position = position_dodge(.2), linewidth = 2, stroke = 2) +
+  # add labels by x-axis
+  geom_text(data = tibble(octave_steps = 12, FA_percent_detailed = 0,
+                          tone = "No Go", genotype = "WT", line = "Fmr1", detail = "Normal"),
+            aes(label = tone), size = 5, show.legend = FALSE, vjust = 2, hjust = -0.2,
+            family = "EconSansCndReg") +
+  geom_text(data = tibble(octave_steps = 1, FA_percent_detailed = 0,
+                          tone = "Go", genotype = "WT", line = "Fmr1", detail = "Normal"),
+            aes(label = tone), size = 4, show.legend = FALSE, vjust = 3.1, hjust = 4,
+            family = "EconSansCndReg") +
+  coord_cartesian(clip = "off") +
+  # table on graph
+  # annotate(geom = "table", x = 12, y = 100,
+  #          label = list(filter(discrimination_FA_table, rat_ID %in% BG_rats)),
+  #          table.theme = ttheme_gtplain(
+  #            padding = unit(c(1, 0.75), "char")
+  #          )) +
+  scale_x_continuous(breaks = seq(0, 12, by = 2)) +
+  scale_y_continuous(limits = c(0, 100)) +
+  scale_shape_manual(values = c("Tsc2" = 21, "Fmr1" = 24)) +
+  scale_fill_manual(values = c("Tsc2" = "slategrey", "Fmr1" = "tan4")) +
+  scale_color_manual(values = c("WT" = "black", "Het" = "blue", "KO" = "red")) +
+  labs(x = "Octave Step",
+       y = "False Alarm %",
+       title = "Discrimination across an octave",
+       fill = "Line", shape = "Line",
+       color = "Genotype") +
+  theme_ipsum_es() +
+  guides(colour = guide_legend(override.aes = list(linewidth = 1))) +
+  theme(legend.key.width = unit(1.25,"cm"))
+
 
 # Individual Rxn Graphs -------------------------------------------------------
 
