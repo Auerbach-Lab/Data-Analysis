32147eeeffe37e540075317fae40f8b93833ef46
Noelle James <github@nohelix.com>
2025-05-02 16:34:07 -0500

    Moved Data into data out of main
    Should consider an analysis.R but its ok.

--- FXS x TSC/data.R
+++ FXS x TSC/data.R
@@ -1,6 +1,44 @@
 # Get core data -----------------------------------------------------------
 source("main.R")
 
+# Load Necessary Datasets -------------------------------------------------
+cat("Loading data.\n")
+load(paste0(projects_folder, "run_archive.Rdata"), .GlobalEnv)
+rat_decoder = fread(glue("{projects_folder}/rat_archive.csv"),
+                    select = c("Rat_ID", "DOB", "Sex", "Genotype", "HL_date"))
+
+# Remove bad data
+#TODO: deal with multiple runs in a day
+dataset = run_archive %>% 
+  # Omit Invalid runs
+  filter(invalid != "TRUE") %>%
+  #Omit runs with wrong delay window, the negate means it returns non-matches
+  #ISSUE: gives warning because it expects a vector not a dataframe 
+  filter(str_detect(warnings_list, pattern = "wrong delay window", negate = TRUE))
+
+# Get core data -----------------------------------------------------------
+core_columns = c("date", "rat_name", "rat_ID", "genotype", "sex",
+                 "file_name", "experiment", "phase", "task", "detail", 
+                 "stim_type", "analysis_type", "block_size", "complete_block_count", 
+                 "dprime", "reaction", "FA_percent")
+
+core_data = dataset %>% 
+  # Get essential columns in usable form; expands the dataframe
+  unnest_wider(assignment) %>% 
+  # Only keep relevant Experiments
+  filter(experiment %in% c("FXS x TSC")) %>%
+  left_join(rat_decoder, by = join_by(rat_ID == Rat_ID)) %>%
+  rename(sex = Sex) %>%
+  mutate(FXS = str_extract(Genotype, pattern = "Fmr1_[:alpha:]+") %>% str_remove("Fmr1_"),
+         TSC = str_extract(Genotype, pattern = "Tsc2_[:alpha:]+") %>% str_remove("Tsc2_"),
+         genotype = case_when(FXS == "WT" & TSC == "WT" ~ "Wild-type",
+                              FXS == "KO" & TSC == "WT" ~ "FXS only",
+                              FXS == "WT" & TSC == "Het" ~ "TSC only",
+                              FXS == "KO" & TSC == "Het" ~ "Double KO")) %>%
+  unnest_wider(stats) %>%
+  select(all_of(core_columns))
+
+
 # Calculate Overall TH ----------------------------------------------------
 cat("Caluclating: thresholds...")
 
